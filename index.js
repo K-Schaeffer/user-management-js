155fccf04a0148ca6dc21b82c4f5be0aa4dbfef6
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-24 10:23:21 -0300

    Using forEach with JSON and testing the form event

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {}; // My JSON
 
-fields.forEach(function (field, index) {
+document.querySelector("#form-user-create").addEventListener("submit", function (event) {
+   
+    event.preventDefault(); //Canceling the default comportament
 
-    if (field.name == "gender") {
-        if (field.checked) {
-            user[field.name] = field.value; //Name of the JSON property / Value of it
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value; //Name of the JSON property / Value of it
+            }
+        } else {
+            user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
+
+    });
+
+    console.log(user);
 
 });
 
-console.log(user);
