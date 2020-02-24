b4afbb39a7c6756151d7b41f8fc52dbe7640518e
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-24 09:32:54 -0300

    Going through the array of fields

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var passowrd = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+        if (field.checked) {
+            console.log("Sim", field);
+        }
+    } else {
+        console.log("Não");
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index);
+
+})
